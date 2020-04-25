2b2b8d8609dcf9b1696143d0a1228e823f8351a7
Pablo <pablotoledosegundo@gmail.com>
2020-04-25 15:22:17 +0200

    Commit nº16

--- pd110/src/boletin/views.py
+++ pd110/src/boletin/views.py
@@ -1,14 +1,21 @@
 from django.shortcuts import render
 
 from .forms import RegForm
+from .models import Registrado
 
 # Create your views here.
 def inicio(request):
 	form = RegForm(request.POST or None)
 	if form.is_valid():
 		form_data = form.cleaned_data
-		print (form_data.get("nombre"))
-		print (form_data.get("edad"))
+		campo_email =  form_data.get("email")
+		campo_nombre = form_data.get("nombre")
+		objeto = Registrado.objects.create(email=campo_email, nombre=campo_nombre)
+
+		#obj = Registrado
+		#obj.email = campo_email
+		#obj.save()
+
 	context = {
 		"el_form": form,
 	}
