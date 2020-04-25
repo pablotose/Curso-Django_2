d1683353401cfaa453be9ae3d6b22392438219a7
Pablo <pablotoledosegundo@gmail.com>
2020-04-25 17:52:56 +0200

    Commit nº18
    Correo valido con extensión

--- pd110/src/boletin/forms.py
+++ pd110/src/boletin/forms.py
@@ -8,6 +8,19 @@ class RegModelForm(forms.ModelForm):
 		model = Registrado
 		fields = ["nombre", "email"]
 
+	def clean_email(self):
+		email =  self.cleaned_data.get("email")
+		email_base, proveeder = email.split("@")
+		dominio, extension = proveeder.split(".")
+		if not extension == "edu":
+			raise forms.ValidationError("Por favor utiliza un correo del IES Fernando Aguilar")
+		return email
+
+	def clean_nombre(self):
+		nombre = self.cleaned_data.get("nombre")
+		#Validaciones
+		return nombre
+
 class RegForm(forms.Form):
 	nombre = forms.CharField(max_length=100)
 	email = forms.EmailField()
