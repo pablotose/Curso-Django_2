0d107e8eae7203bd92a41a4416d935d5b8a150ec
Pablo <pablotoledosegundo@gmail.com>
2020-04-25 18:23:10 +0200

    Commit nº19

--- pd110/src/boletin/views.py
+++ pd110/src/boletin/views.py
@@ -5,6 +5,9 @@ from .models import Registrado
 
 # Create your views here.
 def inicio(request):
+	titulo= "HOLA"
+	if request.user.is_authenticated:
+		titulo = "Bienvenido %s" %(request.user)
 	form = RegForm(request.POST or None)
 	if form.is_valid():
 		form_data = form.cleaned_data
@@ -17,6 +20,7 @@ def inicio(request):
 		#obj.save()
 
 	context = {
-		"el_form": form,
+		"titulo" : titulo,
+		"el_form" : form,
 	}
 	return render(request, "inicio.html", context)
